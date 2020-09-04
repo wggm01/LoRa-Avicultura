82e3b14294c433946edb1c74475d9948aef20573
Wvaldo Graell <wvaldo.graell@utp.ac.pa>
2020-09-03 19:06:40 -0500

    resolviendo problemas

---
+++ mqttclient.py
@@ -0,0 +1,14 @@
+import paho.mqtt.client as mqtt
+broker_addr = "jiclora.duckdns.org"
+port = 8883
+capath = r'etc/ssl/certs'
+#capath = r'C:\Users\wggm\OneDrive\Contribuciones\JIC2020\certs'
+client = mqtt.Client("legiony7")
+client.username_pw_set(username="pi",password="raspberry")
+client.tls_set(capath,tls_version=2)
+
+client.connect(broker_addr,port)
+for i in range(10):
+    client.subscribe("world")
+    client.publish("world","SSL FUNCIONANDO")
+client.disconnect()
