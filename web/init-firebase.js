4fb01b2daab5f222a6af24f41eeb9d6e79904a12
Wvaldo Graell <wvaldo.graell@utp.ac.pa>
2020-10-11 15:00:51 -0500

    Hosting y deploy completado. pero el archivo de configuracion esta expuesto...no se porque

---
+++ web/init-firebase.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBqnX0Ef7xA6TCaBRnm-ycB5iUmyklsLLI",
+    authDomain: "avicultura-lora.firebaseapp.com",
+    databaseURL: "https://avicultura-lora.firebaseio.com",
+    projectId: "avicultura-lora",
+    storageBucket: "avicultura-lora.appspot.com",
+    messagingSenderId: "267056910461",
+    appId: "1:267056910461:web:737a8259f17701d449a114"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
